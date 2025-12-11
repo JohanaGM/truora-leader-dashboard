18338f5235db95667a02bf7bc878c534131da8f3
JohanaGM <johaga.95@hotmail.es>
2025-12-10 22:53:06 -0500

    Fix: usar Production URL de N8N

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  n8nWebhookUrl: 'https://n8n.zapsign.com.br/webhook-test/d4d32508-4178-4d90-bfe9-c3f6c5889bfc', //Cambiar ambiente cuando se suba a hosting
+  n8nWebhookUrl: 'https://n8n.zapsign.com.br/webhook/d4d32508-4178-4d90-bfe9-c3f6c5889bfc', // Production URL
   supabase: {
     url: 'https://fwgebmdqjlzsvbbtrdkm.supabase.co',
     key: 'sb_publishable_cFZeeY3KJRYjRXA4L61qbg_nDny1VfB'
