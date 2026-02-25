adf89f635ab417b0c0110f6f94526b5a92482207
JohanaGM <johaga.95@hotmail.es>
2026-02-25 12:49:07 -0500

    url produccionchat

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   n8nWebhookUrl: 'https://n8n.zapsign.com.br/webhook/d4d32508-4178-4d90-bfe9-c3f6c5889bfc', // Production URL
+  n8nChatWebhookUrl: 'https://n8n.zapsign.com.br/webhook/chat-ia-tips', // Production URL
   supabase: {
     url: 'https://fwgebmdqjlzsvbbtrdkm.supabase.co',
     key: 'sb_publishable_cFZeeY3KJRYjRXA4L61qbg_nDny1VfB'
