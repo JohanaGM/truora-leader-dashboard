2b738a24a1c00afcb47936755fe736602e3bba09
JohanaGM <johana.95@hotmail.es>
2026-03-25 00:55:08 -0500

    se camnio la consulta de ia del tip a url test de n8n par apruebasS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   n8nWebhookUrl: 'https://n8n.zapsign.com.br/webhook/d4d32508-4178-4d90-bfe9-c3f6c5889bfc',
   n8nWebhookUrlTest: 'https://n8n.zapsign.com.br/webhook-test/d4d32508-4178-4d90-bfe9-c3f6c5889bfc',
   n8nChatWebhookUrl: 'https://n8n.zapsign.com.br/webhook/chat-ia-tips',
+  n8nChatWebhookUrlTest: 'https://n8n.zapsign.com.br/webhook-test/chat-ia-tips',
   supabase: {
     url: 'https://fwgebmdqjlzsvbbtrdkm.supabase.co',
     key: 'sb_publishable_cFZeeY3KJRYjRXA4L61qbg_nDny1VfB'
