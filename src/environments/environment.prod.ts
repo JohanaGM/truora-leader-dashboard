bdeae5e5f8720157392e62a1217acfc8db14753e
JohanaGM <johana.95@hotmail.es>
2026-04-21 16:36:40 -0500

    cambio urlprodtruface

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   n8nWebhookUrl: 'https://n8n.zapsign.com.br/webhook/d4d32508-4178-4d90-bfe9-c3f6c5889bfc', // Production URL
   n8nChatWebhookUrl: 'https://n8n.zapsign.com.br/webhook/chat-ia-tips', // Production URL
-  n8nTelegramWebhookUrl: 'https://n8n.zapsign.com.br/webhook/telegram-to-sheets',
+  n8nTelegramWebhookUrl: 'https://n8n.zapsign.com.br/webhook/truface-upload',// production
   n8nSnowflakeWebhookUrl: 'https://n8n.zapsign.com.br/webhook/snowflake-query',
   supabase: {
     url: 'https://fwgebmdqjlzsvbbtrdkm.supabase.co',
